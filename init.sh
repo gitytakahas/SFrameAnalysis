bca91bcd6705f20aeacbd21ab841ac5a8e810d1d
Yuta Takahashi <Yuta.Takahashi@cern.ch>
2016-09-23 10:08:03 +0200

    add LepEff scaling factor

--- init.sh
+++ init.sh
@@ -3,9 +3,19 @@
 echo "Creating a few directories"
 mkdir AnalysisTemp
 mkdir AnalysisOut
-echo "Checking out SFrame from SVN"
-svn co svn://svn.code.sf.net/p/sframe/code/SFrame/trunk SFrame
-echo "Getting submodules"
-git submodule init
-git submodule update
-echo "Remember to setup and compile SFrame"
+
+cd SFrame/
+source setup.sh
+cd -;
+
+echo "Compiling sub-directories"
+for dir in BTaggingTools NtupleVariables PileupReweightingTool SFrame TauTauResonances GoodRunsLists
+do
+    echo
+    echo $dir
+    echo
+    cd $dir
+    make distclean
+    make
+    cd -
+done
