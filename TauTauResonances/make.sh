2d9931b47957cc195fcf6c5234bdbbddffd09557
Yuta Takahashi <Yuta.Takahashi@cern.ch>
2016-09-23 09:34:42 +0200

    cleaning

--- TauTauResonances/make.sh
+++ TauTauResonances/make.sh
@@ -1,1 +1,3 @@
-make distclean && make && sframe_main config/TauTauAnalysis_config.xml 2>&1 | tee log
+#make distclean && make && sframe_main config/TauTauAnalysis_config_miniAODv1.xml 2>&1 | tee log
+make distclean && make && sframe_main config/TauTauAnalysis_config_miniAODv2.xml 2>&1 | tee log
+#sframe_main config/TauTauAnalysis_config_miniAODv2.xml 2>&1 | tee log
