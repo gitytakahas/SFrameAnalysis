715665e1e48732f64d5e3d31a5d769c6e74f093b
Izaak Neutelings (UniZ) [ineuteli] <ineuteli@t3ui03.psi.ch>
2016-11-21 10:48:18 +0100

    HTT scaling factors

--- LepEff2016/src/ScaleFactorTool.cxx
+++ LepEff2016/src/ScaleFactorTool.cxx
@@ -10,11 +10,11 @@ ScaleFactorTool::ScaleFactorTool(SCycleBase* parent, const char* name ):
 {
   SetLogName( name );
   DeclareProperty( m_name+"Mu_TrigFile ",   m_Mu_TrigFile       = std::string (std::getenv("SFRAME_DIR")) + "/../LepEff2016/data/Muon/Run2016BCD/Muon_IsoMu22_OR_TkIsoMu22_eff.root" ); 
-  DeclareProperty( m_name+"Mu_IdIsoFil",    m_Mu_IdIsoFile      = std::string (std::getenv("SFRAME_DIR")) + "/../LepEff2016/data/Muon/Run2016BCD/Muon_IdIso0p20_eff.root" );
+  DeclareProperty( m_name+"Mu_IdIsoFil",    m_Mu_IdIsoFile      = std::string (std::getenv("SFRAME_DIR")) + "/../LepEff2016/data/Muon/Run2016BCD/Muon_IdIso0p15_eff.root" );
   //DeclareProperty( m_name+"Mu_IdFile",      m_Mu_IdFile         = std::string (std::getenv("SFRAME_DIR")) + "/../LepEff2016/data/Muon/MuonIso_Z_RunBCD_prompt80X_7p65.root" );
   //DeclareProperty( m_name+"Mu_IsoFile",     m_Mu_IsoFile        = std::string (std::getenv("SFRAME_DIR")) + "/../LepEff2016/data/Muon/MuonIso_Z_RunBCD_prompt80X_7p65.root" );
   DeclareProperty( m_name+"EleTrigFile",    m_Ele_TrigFile      = std::string (std::getenv("SFRAME_DIR")) + "/../LepEff2016/data/Electron/Run2016BCD/Electron_Ele25eta2p1WPTight_eff.root" );
-  DeclareProperty( m_name+"EleIdIsoName",   m_Ele_IdIsoFile     = std::string (std::getenv("SFRAME_DIR")) + "/../LepEff2016/data/Electron/Run2016BCD/Electron_IdIso0p10_eff.root" );
+  DeclareProperty( m_name+"EleIdIsoName",   m_Ele_IdIsoFile     = std::string (std::getenv("SFRAME_DIR")) + "/../LepEff2016/data/Electron/Run2016BCD/Electron_IdIso0p15_eff.root" );
   //DeclareProperty( m_name+"Ele_IdFile",     m_Ele_IdFile        = std::string (std::getenv("SFRAME_DIR")) + "/../LepEff2016/data/Electron/egammaEffi_txt_SF2D.root" );
   //DeclareProperty( m_name+"Ele_IsoFile",    m_Ele_IsoFile       = std::string (std::getenv("SFRAME_DIR")) + "/../LepEff2016/data/Electron/egammaEffi_txt_SF2D.root" );
 }
